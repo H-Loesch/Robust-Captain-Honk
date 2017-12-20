560d9a88426901facc90ce5e61d71a0104c98d27
Hunter-loesch <h38383838@tamu.edu>
2017-12-19 22:44:17 -0600

    Converted the actor object fromt a struct into a class. Got the obvious actor functions declared, moving to defining next.

---
+++ Robust Captain Honk/Actor.cpp
@@ -0,0 +1,7 @@
+#include "Actor.h"
+
+Actor::Actor() {
+	health = 45;
+	magic = 30;
+	name = "PlaceHolder";
+}
--- Robust Captain Honk/Actor.h
+++ Robust Captain Honk/Actor.h
@@ -1,9 +1,46 @@
 #pragma once
 #include <string>
 
-struct Actor
-{
+class Actor{
+private: 
 	int health;
 	int magic;
 	std::string name = "player";
-};
+	
+	//Describes the min and max outcomes for various actions
+	//these are unique to the actor, so they have to be stored in object
+	int damageLow;
+	int damageHigh;
+	int healLow;
+	int healHigh;
+	int rechargeLow;
+	int rechargeHigh;
+	int stealLow;
+	int stealHigh;
+
+public:
+
+	Actor();
+
+	Actor(int healthStart, int magicStart, std::string Name);
+
+	//decreases enemy health
+	void attack(Actor* enemy);
+
+	//increases health, decreases magic
+	void heal();
+
+	//increases magic
+	void recharge();
+
+	void steal();
+
+	//writes a statement to the user
+	void UIOutput();
+
+	int getHealth();
+	int getMagic();
+	int getName();
+}
+
+//TODO: will the various min/max values need getters?
